84435acc246353ec48ac1e04e893b451dd51f90b
Srigi <295197+srigi@users.noreply.github.com>
2025-05-10 01:40:09 +0200

    Add minimal React setup

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,31 +1,12 @@
-/**
- * This file will automatically be loaded by vite and run in the "renderer" context.
- * To learn more about the differences between the "main" and the "renderer" context in
- * Electron, visit:
- *
- * https://electronjs.org/docs/tutorial/process-model
- *
- * By default, Node.js integration in this file is disabled. When enabling Node.js integration
- * in a renderer process, please be aware of potential security implications. You can read
- * more about security risks here:
- *
- * https://electronjs.org/docs/tutorial/security
- *
- * To enable Node.js integration in this file, open up `main.ts` and enable the `nodeIntegration`
- * flag:
- *
- * ```
- *  // Create the browser window.
- *  mainWindow = new BrowserWindow({
- *    width: 800,
- *    height: 600,
- *    webPreferences: {
- *      nodeIntegration: true
- *    }
- *  });
- * ```
- */
+import React from 'react';
+import { jsx as _jsx } from 'react/jsx-runtime';
+import { createRoot } from 'react-dom/client';
 
+import { App } from './App';
 import './index.css';
 
-console.log('👋 This message is being logged by "renderer.ts", included via Vite');
+createRoot(document.body).render(
+  _jsx(React.StrictMode, {
+    children: _jsx(App, {}),
+  }),
+);
